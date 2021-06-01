3f2564ea68685bef033a25ebcde9f7f2a0f0d28d
LUCASRENAA <lucasrenaa1996@gmail.com.br>
2021-05-31 23:06:19 -0300

    alterando settings

--- padrao/wsgi.py
+++ padrao/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controle_estoque.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'padrao.settings')
 
 application = get_wsgi_application()
